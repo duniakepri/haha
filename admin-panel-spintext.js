d7e08ecff1f69b63738f19f68eb180e5d229150d
duniakepri <49114700+duniakepri@users.noreply.github.com>
2019-03-31 14:27:39 +0700

    Add files via upload

---
+++ admin-panel-spintext.js
@@ -0,0 +1,9 @@
+<script language="JavaScript">
+var password;
+var pass1="bangsat";
+password=prompt('Masukkan password untuk membuka halaman ini!',' ');
+if (password==pass1) alert('Password Benar! Klik OK untuk membuka halaman ini!');
+else {
+  window.location="https://spintext-jagoankode.blogspot.com/p/spin-text-to-uniqe.html";
+}
+</script>
